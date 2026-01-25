9fae32ecd1cdf8f637812781e0284d8d5fdbf2e3
jacquesbilombe <jamesbilombe24@gmail.com>
2026-01-25 12:59:47 -0300

    new update

--- socialAPP/main.py
+++ socialAPP/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from routers.poost import router as post_router
+
+from socialAPP.routers.post import router as post_router
 
 app = FastAPI()
 app.include_router(post_router)
---
+++ socialAPP/tests/conftest.py
@@ -0,0 +1,30 @@
+from typing import AsyncGenerator, Generator
+
+import pytest
+from fastapi.testclient import TestClient
+from httpx import AsyncClient
+
+from socialAPP.main import app
+from socialAPP.routers.post import comment_table, post_table
+
+
+@pytest.fixture(scope="session")
+def anyio_backend() -> str:
+    return "asyncio"
+
+
+@pytest.fixture()
+def client() -> Generator:
+    with TestClient(app) as c:
+        yield c
+
+@pytest.fixture(autouse=True)
+async def db() -> AsyncGenerator:
+    post_table.clear()
+    comment_table.clear()
+    yield
+
+@pytest.fixture()
+async def async_client(client) -> AsyncGenerator:
+    async with AsyncClient(app=app, base_url=client.base_url) as c:
+        yield c
